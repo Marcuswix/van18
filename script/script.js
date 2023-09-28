0afa1fc282b9599f929b2aa8750ed47aca82a83f
Marcus <Marcuswix@yahoo.se>
2023-09-28 09:21:56 +0200

    Animations added

---
+++ script/script.js
@@ -0,0 +1,22 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); // Ändra bild var 5 sekunder (5000 ms)

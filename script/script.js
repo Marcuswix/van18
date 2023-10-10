e4ea65d60b843ab5bbfe7c7e66e752741190cf2e
Marcus <Marcuswix@yahoo.se>
2023-10-10 14:21:37 +0200

    Har lagt till en 'top-the-top'-knapp

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// Slideshow
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 
@@ -20,4 +21,31 @@ function nextSlide() {
     showSlide(currentSlide);
 }
 
-setInterval(nextSlide, 8000); // Ändra bild varaktighet
+setInterval(nextSlide, 8000);
+
+// Function - To the top
+function toTop()
+{
+    window.scrollTo(
+        {
+            top: 0,
+            behavior: "smooth"
+        }
+    )
+}
+
+// Hide "To the top" - button
+window.addEventListener("scroll", function()
+{
+    let element = document.getElementById("btn-top")
+
+    if (window.scrollY > 100)
+    {
+        element.classList.remove("hide-btn")
+    }
+
+    else
+    {
+        element.classList.add("hide-btn")
+    }
+})
